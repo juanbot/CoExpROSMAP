dfa9dd4d9368b4ea2d0e27e286299cea60324509
Juan A. Botia <juanbot@um.es>
2019-01-16 08:56:07 +0000

    1ST COMMIT

---
+++ R/main.R
@@ -0,0 +1,35 @@
+
+#' initDb - Initialization of the package so the ROSMAP networks can be used with
+#' CoExpNets
+#'
+#'
+#' @param mandatory
+#'
+#' @return
+#' @export
+#'
+#' @examples
+initDb = function(mandatory=F){
+  the.dir = system.file("", "extdata", package = "CoExpROSMAP")
+  tissues = c("notad","probad","ad","allsamples")
+  nets = c("netnotad.8.it.50.rds",
+           "netprobad.11.it.50.rds",
+           "netad.8.it.50.rds",
+           "netROSMAPSingle.6.it.50.rds")
+  residuals = c("fpkm.casectrl.qc.qn.combat.covs.svas2.res.cogdx.notad.rds",
+                "fpkm.casectrl.qc.qn.combat.covs.svas2.res.cogdx.probad.rds",
+                "fpkm.casectrl.qc.qn.combat.covs.svas2.res.cogdx.ad.rds",
+                "fpkm.casectrl.qc.qn.combat.covs.svas2.res.rds")
+  for(tissue in tissues){
+    net = nets[which(tissues == tissue)]
+    residual = residuals[which(tissues == tissue)]
+    CoExpNets::addNet(which.one="rosmap",
+           tissue=tissue,
+           netfile=net,
+           ctfile=paste0(the.dir,"/",net,"_celltype.csv"),
+           gofile=paste0(the.dir,"/",net,"_gprof.csv"),
+           exprdatafile=paste0(the.dir,"/",
+                              residual,".resids.rds"),
+           overwrite=mandatory)
+  }
+}
